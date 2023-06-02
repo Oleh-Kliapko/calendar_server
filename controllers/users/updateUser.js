bf3ddd7520676c6b3dd5e75630f4e149827f47c7
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-02 20:05:35 +0300

    fix

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -5,15 +5,27 @@ const { HttpError } = require('../../helpers');
 
 const updateUser = async (req, res) => {
   const { userId } = req.params;
-  const result = await User.findByIdAndUpdate(userId, req.body, {
+  const user = await User.findByIdAndUpdate(userId, req.body, {
     new: true,
   });
 
-  if (!result) {
+  if (!user) {
     throw HttpError(404, 'Not found');
   }
 
-  return res.status(200).json(result);
+  const { avatarURL, username, email, birthday, phone, skype, token } = user;
+
+  return res.status(200).json({
+    data: {
+      avatarURL,
+      username,
+      email,
+      birthday,
+      phone,
+      skype,
+      token,
+    },
+  });
 };
 
 module.exports = updateUser;
