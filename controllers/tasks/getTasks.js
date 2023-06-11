09c8b0c700bf3678e617ce380de01651eb5beff4
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-11 10:51:11 +0300

    add boards of tasks

--- controllers/tasks/getTasks.js
+++ controllers/tasks/getTasks.js
@@ -8,6 +8,32 @@ module.exports = async (req, res) => {
 
   const result = await Task.find({ owner }, '-updatedAt');
 
+  const boards = [
+    { category: 'to-do', owner: '', tasks: [] },
+    { category: 'in-progress', owner: '', tasks: [] },
+    { category: 'done', owner: '', tasks: [] },
+  ];
+
+  result.forEach(task => {
+    const boardIndex = boards.findIndex(
+      board => board.category === task.category,
+    );
+    if (boardIndex !== -1) {
+      const taskWithOwner = {
+        _id: task._id,
+        title: task.title,
+        start: task.start,
+        end: task.end,
+        priority: task.priority,
+        date: task.date,
+      };
+      boards[boardIndex].tasks.push(taskWithOwner);
+      if (!boards[boardIndex].owner) {
+        boards[boardIndex].owner = task.owner;
+      }
+    }
+  });
+
   const tasksCurrentMonth = result.filter(
     item => item.date.getMonth() + 1 === Number(month),
   );
@@ -16,6 +42,7 @@ module.exports = async (req, res) => {
     data: {
       tasksCurrentMonth,
       allTasks: result,
+      boards,
     },
   });
 };
