e817b689c1072563af35cd51b0130005f4a407ce
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-11 19:09:35 +0300

    Update patterns.js

--- helpers/patterns.js
+++ helpers/patterns.js
@@ -3,7 +3,7 @@ const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const passwordPattern = /^.*(?=.{6,})((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/; // 6 characters, at least one upperCase and one lowercase
 const urlPattern =
   /^https?:\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/;
-const phonePattern = /^38 \(\d{3}\) \d{3} \d{2} \d{2}$/;
+const phonePattern = /^\+380\d{9}$/;
 const timePattern = /^(?:[01]\d|2[0-3]):(?:[0-5]\d)$/;
 const datePattern = /^\d{4}-\d{2}-\d{2}$/;
 
